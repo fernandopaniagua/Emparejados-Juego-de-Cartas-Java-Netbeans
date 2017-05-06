dc5c05ef9b7018767e72992dfcd5d7816cb56d80
lafabricadeandroides <fernando.paniagua@gmail.com>
2017-05-06 15:36:00 +0200

    Version 1.0

---
+++ src/net/juanxxiii/emparejados/Carta.java
@@ -0,0 +1,27 @@
+package net.juanxxiii.emparejados;
+
+import javax.swing.JLabel;
+
+/**
+ * Representa una carta, compuesto por un JLabel y un identificador de posición
+ * 
+ * @author FPA
+ */
+public class Carta extends JLabel {
+    private int pos;
+    /**
+     * Constructor
+     * 
+     * @param pos Posición dentro de la partida de juego 
+     */
+    public Carta(int pos) {
+        super();
+        this.pos = pos;
+    }
+    public int getPos() {
+        return pos;
+    }
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+}
